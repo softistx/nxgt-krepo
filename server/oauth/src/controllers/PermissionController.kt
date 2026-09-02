9657b160501457ecc0a7fdbfa6625e6aaa52e317
Steve Tsala <stevegires@gmail.com>
2026-09-02 19:06:59 -0400

    update: Update oauth modeule

--- server/oauth/src/controllers/PermissionController.kt
+++ server/oauth/src/controllers/PermissionController.kt
@@ -3,6 +3,7 @@ package com.softistx.oauth.controllers
 import com.softistx.graphix.schema.Argument
 import com.softistx.graphix.schema.MutationMapping
 import com.softistx.graphix.schema.QueryMapping
+import com.softistx.oauth.graphql.types.CreatePermissionInput
 import com.softistx.oauth.models.Permission
 
 class PermissionController {
@@ -16,6 +17,6 @@ class PermissionController {
 
     @MutationMapping
     suspend fun createPermission(
-        @Argument input: Permission,
+        @Argument input: CreatePermissionInput,
     ): Permission = Permission("4", input.name, input.description)
 }
