30c1cb92ccd707432d9f490b9b2763c5441cf2bc
Steve Tsala <stevegires@gmail.com>
2026-09-02 23:41:55 -0400

    update: Update dependencies

--- server/oauth/src/plugins/DependencyInjection.kt
+++ server/oauth/src/plugins/DependencyInjection.kt
@@ -3,6 +3,7 @@ package com.softistx.oauth.plugins
 import com.softistx.oauth.di.KtorApplication
 import com.strange.api.di.Properties
 import io.ktor.server.application.*
+import org.koin.core.annotation.KoinExperimentalAPI
 import org.koin.core.logger.Level
 import org.koin.dsl.module
 import org.koin.environmentProperties
@@ -10,11 +11,16 @@ import org.koin.ktor.plugin.Koin
 import org.koin.logger.slf4jLogger
 import org.koin.plugin.module.dsl.withConfiguration
 
+@OptIn(KoinExperimentalAPI::class)
 fun Application.configureDependencyInjection() {
     install(Koin) {
         environmentProperties()
         properties(mapOf(Properties.ENVIRONMENT to environment, Properties.APPLICATION to this))
         slf4jLogger(level = Level.INFO)
+        bridge {
+            koinToKtor()
+            ktorToKoin()
+        }
         modules(
             module {
             },
