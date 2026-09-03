7a1c4efca15b3f16280db5d545db7f76aab5e074
Steve Tsala <stevegires@gmail.com>
2026-09-02 20:52:39 -0400

    update: Update graphql

---
+++ server/oauth/src/plugins/GraphQL.kt
@@ -0,0 +1,12 @@
+package com.softistx.oauth.plugins
+
+import com.softistx.graphix.GraphixBuilder
+import com.softistx.oauth.controllers.PermissionController
+
+
+fun GraphixBuilder.registerGraphQLOperations() {
+    val permission = PermissionController()
+
+    query(permission)
+    mutation(permission)
+}
