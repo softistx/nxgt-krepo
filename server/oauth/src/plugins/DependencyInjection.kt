d1c150bc386dbb28edfb578e4086b86cd2bcf996
Steve Tsala <stevegires@gmail.com>
2026-09-02 23:39:06 -0400

    update: Update di

--- server/oauth/src/plugins/DependencyInjection.kt
+++ server/oauth/src/plugins/DependencyInjection.kt
@@ -1,5 +1,6 @@
 package com.softistx.oauth.plugins
 
+import com.softistx.oauth.di.KtorApplication
 import com.strange.api.di.Properties
 import io.ktor.server.application.*
 import org.koin.core.logger.Level
@@ -7,6 +8,7 @@ import org.koin.dsl.module
 import org.koin.environmentProperties
 import org.koin.ktor.plugin.Koin
 import org.koin.logger.slf4jLogger
+import org.koin.plugin.module.dsl.withConfiguration
 
 fun Application.configureDependencyInjection() {
     install(Koin) {
@@ -17,5 +19,6 @@ fun Application.configureDependencyInjection() {
             module {
             },
         )
+        withConfiguration<KtorApplication>()
     }
 }
